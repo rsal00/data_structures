19c5a2069ba4a258b44379ea584285181d8b0275
ruben <136784798+rsal00@users.noreply.github.com>
2025-04-08 20:00:54 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,37 @@
 #include <iostream>
 #include "LinkedList.h"
 #include "Queue.h"
+#include "Stack.h"
 using namespace std;
 
 int main()
 {
-    Queue q;
-    q.enqueue(4);
-    q.enqueue(0);
-    q.enqueue(2);
-    q.enqueue(9);
+    LinkedList li;
 
-    q.dequeue();
+    li.add(1);
+    li.add(2);
+    li.add(3);
+    li.add(4);
+    li.add(5);
 
-    q.enqueue(11);
+    li.remove(1);
+    li.remove(100);
+    li.remove(5);
 
-    cout << q;
+    cout << li << endl;
 
     return 0;
 }
+
+// find missing number
+// int arr[4] = {5, 6, 8, 9};
+
+// int difference = arr[0];
+// for (int i = 0; i < 4; i++)
+// {
+//     if ((arr[i] - i) != difference)
+//     {
+//         cout << "Missing number is " << i + difference << endl;
+//         break;
+//     }
+// }
